19422dd2b57f1ed172724eb6254a8c7fda4c430c
ali.imron <ali.imron@polytron.co.id>
2025-07-23 21:30:18 +0700

    test response json

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.status(200).send({
+    error: 0,
+    message: {
+      count: 1,
+      name: "users",
+      list: [
+        {
+          "name": "ali imron",
+          "age": "28",
+          "address": "Kudus"
+        }
+      ]
+
+    }
+  });
 });
 
 module.exports = router;
